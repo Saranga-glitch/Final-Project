a46f85510492eaaa3302d11478251188f9bd1ae7
Saranga Pathirana <134229229+Saranga-glitch@users.noreply.github.com>
2023-11-05 23:44:23 +0530

    Added a new page

--- src/Pages/OurStore.js
+++ src/Pages/OurStore.js
@@ -1,8 +1,80 @@
 import React from 'react'
+import BreadCrumb from '../components/BreadCrumb';
+import Meta from '../components/Meta';
+
 
 const OurStore = () => {
   return (
-    <div>OurStore</div>
+    <>
+    <Meta title= {"Our Store"} />
+    <BreadCrumb title='Our Store' />
+    <div className="store-wrapper home-wrapper-2 py-5">
+      <div className="container-xxl" >
+        <div className="row">
+          <div className="col-3">
+            <div className="filter-card mb-3">
+              <h3 className="filter-title">Shop By Categories</h3>
+              <div>
+                <ul className="ps-0">
+                  <li>Watch</li>
+                  <li>Tv</li>
+                  <li>Camera</li>
+                  <li>Laptop</li>
+                  <li>Phones</li>
+                </ul>
+              </div>
+            </div>
+            <div className="filter-card mb-3">
+              <h3 className="filter-title">Filter By</h3>
+              <div>
+                <h5 className="sub-title">Availability</h5>
+                <div>
+                <div className="form-check">
+                  <input
+                    className="form-check-input"
+                    type="checkbox"
+                    value=""
+                    id=""
+                  />
+                  <label className="form-check-label" htmlfor="">
+                    In Stock (1)
+                  </label>  
+                </div>
+                <div className="form-check">
+                  <input
+                    className="form-check-input"
+                    type="checkbox"
+                    value=""
+                    id=""
+                  />
+                  <label className="form-check-label" htmlfor="">
+                    Out of Stock (0)
+                  </label>  
+                </div>
+                </div>
+                <h5 className="sub-title">Price</h5>
+                <div>
+                <div className="form-floating mb-3">
+                <input 
+                  type="email" 
+                  className="form-control" 
+                  id="floatingInput" 
+                  placeholder="name@example.com"
+                />
+               <label htmlFor="floatingInput">Email address</label>
+              </div>
+                </div>
+              </div>
+            </div>
+            <div className="filter-card mb-3">
+              <h3 className="filter-title">Product Tags</h3>
+            </div>
+          </div>
+          <div className="col-9"></div>
+        </div>
+      </div>
+    </div>
+    </>
   )
 }
 
